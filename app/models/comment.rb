1fd4f600e49c83fc861eec5383daab7af71c733c
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-05 12:02:44 +0300

    Add the necessary methods to the created models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
+# The Comment class represents a comment made by a user on a post in the blog app.
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # Callback method that is triggered after a comment is saved or destroyed.
+  # It updates the comments_count attribute of the associated post
+  after_save :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  private
+
+  # Private method that updates the comments_count attribute of the associated post.
+  def update_post_comments_counter
+    post.update(comments_count: post.comments.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
+  # Represents a user in the system.
+  # A user can have multiple posts, comments, and likes.
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  # Returns the most recent posts created by the user.
+  # @param limit [Integer] the maximum number of recent posts to return (default: 3)
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
 end
