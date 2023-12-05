ab27f27eaf4c477fa072582d2fe770fe784b039d
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-05 14:08:56 +0300

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,10 @@ class Post < ApplicationRecord
 
   # Callback method that is triggered after a new post is created.
   # It updates the posts_counter attribute of the associated author.
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
+  after_create :update_posts_counter
 
   # Returns the most recent comments for the post.
-  # The number of comments returned can be limited by the 'limit' parameter.
+  # The number of comments returned can be limited by passing a limit parameter.
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
