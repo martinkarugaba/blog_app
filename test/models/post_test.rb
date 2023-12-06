4e99a4ebca3e355a63c1bd350fcbb6cd1b7cda87
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-06 14:27:15 +0300

    Add tests for post validations

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:user_one) # Replace with your fixture or create a user as needed
+    @post = Post.new(
+      title: 'Example Title',
+      author: @user,
+      comments_counter: 0,
+      likes_counter: 0
+    )
+  end
+
+  test 'valid post' do
+    assert @post.valid?
+  end
+
+  test 'title presence' do
+    @post.title = '   '
+    assert_not @post.valid?
+  end
+
+  test 'title length maximum' do
+    @post.title = 'a' * 251
+    assert_not @post.valid?
+  end
+
+  test 'comments_counter numericality' do
+    @post.comments_counter = 'abc'
+    assert_not @post.valid?
+
+    @post.comments_counter = -1
+    assert_not @post.valid?
+
+    @post.comments_counter = 1.5
+    assert_not @post.valid?
+  end
+
+  test 'likes_counter numericality' do
+    @post.likes_counter = 'abc'
+    assert_not @post.valid?
+
+    @post.likes_counter = -1
+    assert_not @post.valid?
+
+    @post.likes_counter = 1.5
+    assert_not @post.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,25 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should be valid with a name and non-negative posts counter" do
-    user = User.new(name: "John Doe", posts_counter: 3)
+  test 'should be valid with a name and non-negative posts counter' do
+    user = User.new(name: 'John Doe', posts_counter: 3)
     assert user.valid?
   end
 
-  test "should be invalid without a name" do
+  test 'should be invalid without a name' do
     user = User.new(posts_counter: 3)
     assert_not user.valid?
     assert_includes user.errors[:name], "can't be blank"
   end
 
-  test "should be invalid with a negative posts counter" do
-    user = User.new(name: "John Doe", posts_counter: -1)
+  test 'should be invalid with a negative posts counter' do
+    user = User.new(name: 'John Doe', posts_counter: -1)
     assert_not user.valid?
-    assert_includes user.errors[:posts_counter], "must be greater than or equal to 0"
+    assert_includes user.errors[:posts_counter], 'must be greater than or equal to 0'
   end
 
-  test "should be valid with a zero posts counter" do
-    user = User.new(name: "John Doe", posts_counter: 0)
+  test 'should be valid with a zero posts counter' do
+    user = User.new(name: 'John Doe', posts_counter: 0)
     assert user.valid?
   end
 end
