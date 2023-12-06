83f8c7566c56c0a497a46f4daa0b4eb1092e1a07
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-06 20:21:35 +0300

    Add unit tests for posts model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,40 +1,73 @@
-# # test/models/user_test.rb
+require 'test_helper'
 
-# require 'test_helper'
+class PostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:john_doe)
+  end
 
-# class UserTest < ActiveSupport::TestCase
-#   test 'should be valid with a name' do
-#     user = User.new(name: 'John Doe', posts_counter: 1)
-#     assert user.valid?
-#   end
+  test 'should be valid with a title, comments_counter, and likes_counter' do
+    post = Post.new(
+      title: 'Sample Title',
+      comments_counter: 2,
+      likes_counter: 5,
+      author: @user
+    )
+    assert post.valid?, 'Post is not valid'
+  end
 
-#   test 'should be invalid without a name' do
-#     user = User.new(posts_counter: 1)
-#     assert_not user.valid?
-#     assert_equal ['can\'t be blank'], user.errors[:name]
-#   end
+  test 'should not be valid without a title' do
+    post = Post.new(comments_counter: 2, likes_counter: 5, author: @user)
+    assert_not post.valid?, 'Post is valid without a title'
+  end
 
-#   test 'should be invalid with a non-integer posts_counter' do
-#     user = User.new(name: 'John Doe', posts_counter: 'abc')
-#     assert_not user.valid?
-#     assert_equal ['must be an integer'], user.errors[:posts_counter]
-#   end
+  test 'should not be valid with a title exceeding the maximum length' do
+    post = Post.new(
+      title: 'a' * 251,
+      comments_counter: 2,
+      likes_counter: 5,
+      author: @user
+    )
+    assert_not post.valid?, 'Post is valid with a title exceeding the maximum length'
+  end
 
-#   test 'should be invalid with a negative posts_counter' do
-#     user = User.new(name: 'John Doe', posts_counter: -1)
-#     assert_not user.valid?
-#     assert_equal ['must be greater than or equal to 0'], user.errors[:posts_counter]
-#   end
+  test 'should not be valid with a negative comments_counter' do
+    post = Post.new(title: 'Sample Title', comments_counter: -1, likes_counter: 5, author: @user)
+    assert_not post.valid?, 'Post is valid with a negative comments_counter'
+  end
 
-#   test 'should create user with associated comments' do
-#     user = User.new(name: 'John Doe', posts_counter: 1)
-#     user.save
+  test 'should not be valid with a negative likes_counter' do
+    post = Post.new(title: 'Sample Title', comments_counter: 2, likes_counter: -5, author: @user)
+    assert_not post.valid?, 'Post is valid with a negative likes_counter'
+  end
 
-#     post = user.posts.create(title: 'Sample Post', text: 'This is a sample post.')
-#     comment = post.comments.create(user: user, text: 'This is the first comment.')
+  test 'should be valid with zero comments_counter and likes_counter' do
+    post = Post.new(title: 'Sample Title', comments_counter: 0, likes_counter: 0, author: @user)
+    assert post.valid?, 'Post is not valid with zero comments_counter and likes_counter'
+  end
 
-#     assert user.valid?
-#     assert post.valid?
-#     assert comment.valid?
-#   end
-# end
+  test 'should have recent comments in descending order' do
+    post = posts(:first_post)
+    recent_comments = post.recent_comments(5)
+
+    expected_number_of_comments = 5
+
+    assert_equal expected_number_of_comments, recent_comments.length, 'Unexpected number of recent comments'
+
+    recent_comments.each_with_index do |comment, index|
+      if index < expected_number_of_comments - 1
+        assert comment.created_at >= recent_comments[index + 1].created_at, 'Comments are not in descending order'
+      end
+    end
+  end
+
+  test "should update author's posts_counter after creation" do
+    assert_difference('@user.posts_counter', 1) do
+      Post.create(
+        title: 'New Post',
+        comments_counter: 2,
+        likes_counter: 5,
+        author: @user
+      )
+    end
+  end
+end
