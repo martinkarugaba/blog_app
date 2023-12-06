e6e5375bd16c7af6c438df487fc9e41a74d6a34d
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-06 21:12:42 +0300

    Add unit tests for comlike model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,14 +1,14 @@
-# The Like model represents a like given by a user to a post.
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  # after_save :update_post_likes_counter
-  # after_destroy :update_post_likes_counter
+  after_save :update_post_likes_counter
+  after_destroy :update_post_likes_counter
 
   private
 
   def update_post_likes_counter
-    post.update(likes_count: post.likes.count)
+    post.increment!(:likes_counter) if persisted?
+    post.decrement!(:likes_counter) if destroyed?
   end
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,47 @@
+# test/models/like_test.rb
+
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:john_doe)
+    @post = posts(:first_post)
+  end
+
+  test 'should be valid with a user and post' do
+    like = Like.new(user: @user, post: @post)
+    assert like.valid?, 'Like is not valid'
+  end
+
+  test 'should not be valid without a user' do
+    like = Like.new(post: @post)
+    assert_not like.valid?, 'Like is valid without a user'
+  end
+
+  test 'should have an associated user' do
+    like = Like.new(post: @post)
+    assert_raises ActiveRecord::RecordInvalid, 'Validation passed unexpectedly' do
+      like.save!
+    end
+  end
+
+  test 'should have an associated post' do
+    like = Like.new(user: @user)
+    assert_raises ActiveRecord::RecordInvalid, 'Validation passed unexpectedly' do
+      like.save!
+    end
+  end
+
+  test 'should update post likes_counter after creation' do
+    assert_difference('@post.reload.likes_counter', 1) do
+      Like.create(user: @user, post: @post)
+    end
+  end
+
+  test 'should update post likes_counter after destruction' do
+    like = Like.create(user: @user, post: @post)
+    assert_difference('@post.reload.likes_counter', -1) do
+      like.destroy
+    end
+  end
 end
