1baa33aa17d9f1707c74f315485cab8e716f56e3
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-11 16:00:21 +0300

    Add show route for users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-
   root "welcome#index"
 
-  get 'users/:id' => 'users#index', as: 'user'
-
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
