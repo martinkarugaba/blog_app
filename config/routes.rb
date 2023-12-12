c36e10fbe9977b31ed1eedd2b6fbf7eb1b380412
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-12 16:46:39 +0300

    Add unit tests for welcome_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
+  get "/show", to: "welcome#show", as: :show_welcome
+
   resources :users, only: [:index, :show, :new, :create] do
     resources :posts, only: [:index, :show]
   end
