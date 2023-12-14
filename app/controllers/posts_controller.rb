8823816affb5756aa41e7f883b480d2f3effc0ab
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-14 16:47:16 +0300

    Add pagination to all_posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
+require 'will_paginate/array'
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @page = params.fetch(:page, 0).to_i
+    @posts = Post.limit(3).offset(@page * 3)
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
