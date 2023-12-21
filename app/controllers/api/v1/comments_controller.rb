47e06e3412533c8215ad40ef79830e040417fb2d
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-21 20:02:24 +0300

    Fix linter errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -8,22 +8,22 @@ module Api
         render json: @post.comments
       end
 
-      def create        
-        @post = Post.find(params[:post_id])        
-        @comment = @post.comments.create(comment_params)     
-        @comment.user = current_user        
-        if @comment.save          
-          render json: @comment, status: :created       
-        else          
-          render json: @comment.errors, status: :unprocessable_entity        
-        end   
-      end   
+      def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        @comment.user = current_user
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: @comment.errors, status: :unprocessable_entity
+        end
+      end
 
       private
-      def comment_params    
-        params.require(:comment).permit(:content)     
+
+      def comment_params
+        params.require(:comment).permit(:content)
       end
-      
     end
   end
 end
